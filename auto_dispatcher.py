f8c4741eedd6ca9c4a023b6740a22790357d22a5
lujun9972 <lujun9972@gmail.com>
2016-08-11 16:26:28 +0800

    set才支持减法操作

--- auto_dispatcher.py
+++ auto_dispatcher.py
@@ -1,17 +1,18 @@
+import sys
 import os
 import os.path
 import time
 from dispatcher import dispatch_files
 def directory_files(directory):
-    '''Return a list of full paths of files in DIRECTORY.'''
+    '''Return a set of full paths of files in DIRECTORY.'''
     results=[]
     for root,dirs,files in os.walk(directory):
         full_paths=[os.path.join(root,f) for f in files]
         results.extend(full_paths)
-    return results
+    return set(results)
 
 def directory_files_and_size(directory):
-    '''Return a list of full paths and sizes of files in DIRECTORY.'''
+    '''Return a set of full paths and sizes of files in DIRECTORY.'''
     files = directory_files(directory)
     files_and_sizes = set([(f,os.path.getsize(f)) for f in files])
     return files_and_sizes
@@ -23,16 +24,17 @@ def directory_files_until_nochange(directory,interval=5):
     while old != new:
         time.sleep(interval)
         old,new = new,directory_files_and_size(directory)
-    return [file_and_size[0] for file_and_size in new]
+    return set([file_and_size[0] for file_and_size in new])
 
 if __name__ == "__main__":
     try:
         directory = sys.argv[1]
     except IndexError:
         directory = os.getcwd()
+    old = directory_files(directory)
     while True:
-        old = directory_files(directory)
         new = directory_files_until_nochange(directory)
         diff = new - old
-        dispatcher_files(diff)
-        sleep(60)
+        dispatch_files(diff)
+        time.sleep(60)
+        old = new
