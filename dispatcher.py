4b9dcefa4cd5e481f0664dc40a4996fe26b95572
lujun9972 <lujun9972@gmail.com>
2016-08-27 20:04:05 +0800

    Add "StrictHostKeyChecking=no"
    StrictHostKeyChecking=no 表示不询问,直接添加sshd服务器的公钥

--- dispatcher.py
+++ dispatcher.py
@@ -49,13 +49,13 @@ def execute_externel_secure_command(command,password=""):
     return result
 
 def execute_remote_command_by_ssh(host,login,password,command):
-    ssh_command = "ssh {}@{} '{}'".format(login,host,command)
+    ssh_command = "ssh -o StrictHostKeyChecking=no {}@{} '{}'".format(login,host,command)
     return execute_externel_secure_command(ssh_command,password)
 
 import subprocess
 def upload_by_scp (file_path,host,dest_dir,login,password):
     execute_remote_command_by_ssh(host,login,password,"mkdir -p {}".format(dest_dir))
-    scp_command = "scp {0} {1}@{2}:{3}/".format(file_path,login,host,dest_dir,password)
+    scp_command = "scp -o StrictHostKeyChecking=no {0} {1}@{2}:{3}/".format(file_path,login,host,dest_dir,password)
     return execute_externel_secure_command(scp_command,password)
 
 import ftplib
